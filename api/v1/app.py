d089c0519f4890cedd2e4705988f9b6ba8a87342
Ivan <ihk225@gmail.com>
2022-12-08 14:19:21 -0300

    PEP8 corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,10 +11,12 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def destroy(exception):
     """ Destroy session """
     storage.close()
+
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST')
