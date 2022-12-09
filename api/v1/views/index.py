85b9bac6bfb09bf959c65fa12c911fcccfe5f99c
Ivan Hansen <ihk225@gmail.com>
2022-12-09 13:10:38 -0300

    Ex5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 """
 from flask import jsonify
 from api.v1.views.__init__ import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -12,3 +13,10 @@ def status():
         Returns API status
     """
     return jsonify(status="OK")
+
+@app_views.route('/stats', strict_slashes=False)
+def api_stats():
+    """
+        Returns API stats for analytics
+    """
+    return jsonify(storage.count())
