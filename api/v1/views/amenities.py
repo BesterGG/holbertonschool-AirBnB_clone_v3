841aa7f051af347aa8dd53b6d54fef755f6c3c3b
Ivan Hansen <ihk225@gmail.com>
2022-12-11 20:06:08 -0300

    Ex9 test commit

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+    Cities method files
+"""
+from api.v1.views.__init__ import app_views
+from models import storage
+from models.amenity import Amenity
+from flask import jsonify, abort, request
+
+
+@app_views.route('/amenities', methods=['GET', 'POST'])
+def states_gen():
+    """
+        Method to retrieve or generate generic amenities object
+    """
+    req = request.get_json()
+    if request.method == 'POST':
+        status = 400 if not req or 'name' not in req else 201
+        if not req:
+            abort(status, 'Not a JSON')
+        if 'name' not in req:
+            abort(status, 'Missing name')
+        obj = Amenity(**req)
+        obj.save()
+        return jsonify(obj.to_dict()), status
+    if request.method == 'GET':
+        obj_list = [am.to_dict() for am in storage.all("Amenity").values()]
+        return jsonify(obj_list)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET', 'PUT', 'DELETE'])
+def states_scoped(amenity_id):
+    """
+        Method to retrieve/modify/delete a specific state object
+    """
+    met = request.method
+    req = request.get_json()
+    am = amenity = storage.get(Amenity, amenity_id)
+    if am is None:
+        abort(404)
+    if met == 'GET':
+        return jsonify(am.to_dict())
+    if met == 'PUT':
+        status = 404 if not req or 'name' not in req else 200
+        if not req:
+            abort(status, 'Not a JSON')
+        if 'name' not in req:
+            abort(status, 'Missing name')
+        for key, value in req.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(storage.get(Amenity, amenity_id), key, value)
+        storage.save()
+        return jsonify(storage.get(Amenity, amenity_id)), status
+    if met == 'DELETE':
+        storage.delete(storage.get(Amenity, amenity_id))
+        storage.save()
+        return {}, 200
