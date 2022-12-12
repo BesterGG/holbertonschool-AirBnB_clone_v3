b9c7a5cea160bacdc894c5e8da5602f8baab85a3
Ivan Hansen <ihk225@gmail.com>
2022-12-11 23:36:46 -0300

    Minor corrections

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -10,21 +10,23 @@ from flask import jsonify, abort, request
 
 @app_views.route('/users', methods=['GET', 'POST'])
 def users_gen():
-    ''' Retrieves all users or adds a new one '''
-    req = request.get_json()
-    if request.method == 'POST':
-        if not req:
-            abort(400, 'Not a JSON')
-        if 'email' not in req:
-            abort(400, 'Missing email')
-        if 'password' not in req:
-            abort(400, 'Missing password')
-        obj = User(**req)
-        obj.save()
-        return jsonify(obj.to_dict()), 201
+    """
+        Method to retrieve/modify/delete a specific User object
+    """
     if request.method == 'GET':
-        obj_list = [user.to_dict() for user in storage.all("User").values()]
-        return jsonify(obj_list)
+        return jsonify([obj.to_dict() for obj in
+                        storage.all("User").values()])
+    elif request.method == 'POST':
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+        if 'email' not in request.get_json():
+            abort(400, 'Missing email')
+        if 'password' not in request.get_json():
+            abort(400, 'Missing password')
+        new_user = User(**request.get_json())
+        new_user.save()
+        return jsonify(new_user.to_dict()), 201
+
 
 
 @app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
@@ -32,27 +34,24 @@ def users_scoped(user_id):
     """
         Method to retrieve/modify/delete a specific User object
     """
-    met = request.method
-    req = request.get_json()
-    obj_list = [user.to_dict() for user in storage.all("User").values()]
-    if user_id not in [user.get('id') for user in obj_list]:
+    user = storage.get(User, user_id)
+    if user is None:
         abort(404)
-    if met == 'GET':
-        for user in obj_list:
-            if user.get('id') == user_id:
-                return jsonify(user)
-    if met == 'PUT':
-        status = 404 if not req or 'name' not in req else 200
-        if not req:
-            abort(status, 'Not a JSON')
-        if 'name' not in req:
-            abort(status, 'Missing name')
-        for key, value in req.items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(storage.get(User, user_id), key, value)
-        storage.save()
-        return jsonify(storage.get(User, user_id)), status
-    if met == 'DELETE':
-        storage.delete(storage.get(User, user_id))
-        storage.save()
-        return {}, 200
+    else:
+        if request.method == "GET":
+            return jsonify(user.to_dict())
+        if request.method == "DELETE":
+            storage.delete(user)
+            storage.save()
+            return {}, 200
+        elif request.method == "PUT":
+            if not request.get_json():
+                abort(400, 'Not a JSON')
+            else:
+                user = storage.get(User, user_id)
+                for key, value in request.get_json().items():
+                    if key not in ['id', 'created_at',
+                                   'updated_at', 'email']:
+                        setattr(user, key, value)
+                storage.save()
+                return jsonify(user.to_dict()), 200
